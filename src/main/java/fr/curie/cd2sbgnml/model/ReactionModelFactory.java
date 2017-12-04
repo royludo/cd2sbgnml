048e48f6a5b84b88ff987c17c8ee834df3d75a18
royludo <royludo4@hotmail.com>
2017-12-04 11:37:51 +0100

    fix bug introduced in 588c6dcd95ca23e400f06cead8468f1ba0a27754

--- src/main/java/fr/curie/cd2sbgnml/model/ReactionModelFactory.java
+++ src/main/java/fr/curie/cd2sbgnml/model/ReactionModelFactory.java
@@ -2,18 +2,22 @@ package fr.curie.cd2sbgnml.model;
 
 import fr.curie.cd2sbgnml.xmlcdwrappers.ReactionWrapper;
 
+import static fr.curie.cd2sbgnml.xmlcdwrappers.ReactionType.DISSOCIATION;
+import static fr.curie.cd2sbgnml.xmlcdwrappers.ReactionType.HETERODIMER_ASSOCIATION;
+import static fr.curie.cd2sbgnml.xmlcdwrappers.ReactionType.TRUNCATION;
+
 /**
  * A simple factory that creates the right reaction model depending on the reaction type.
  */
 public class ReactionModelFactory {
     public static GenericReactionModel create(ReactionWrapper reactionWrapper) {
-        if(reactionWrapper.getReactionType().equals("HETERODIMER_ASSOCIATION")) {
+        if(reactionWrapper.getReactionType() == HETERODIMER_ASSOCIATION) {
             return new AssociationReactionModel(reactionWrapper);
         }
-        else if(reactionWrapper.getReactionType().equals("DISSOCIATION")) {
+        else if(reactionWrapper.getReactionType() == DISSOCIATION) {
             return new DissociationReactionModel(reactionWrapper);
         }
-        else if(reactionWrapper.getReactionType().equals("TRUNCATION")) {
+        else if(reactionWrapper.getReactionType() == TRUNCATION) {
             return new DissociationReactionModel(reactionWrapper);
         }
         else {
